355612fcdfee29e837c460440cb1a418b4127414
Harrison Katz <git@hkatz.dev>
2022-07-13 12:36:31 -0700

    style: using double quotes consistently

--- src/server.js
+++ src/server.js
@@ -1,29 +1,29 @@
 import Koa from "koa";
 import genWeb from "@srnd/codecup-genericwebsite";
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
 
-const flag = process.env.FLAG || 'test';
+const flag = process.env.FLAG || "test";
 const seed = process.env.SEED || flag;
 const app = new Koa();
 const port = process.env.PORT || 8080;
 const tpl = genWeb.randomTemplate(seed);
 
 app.use(ctx => {
-	if (ctx.method == 'GET') {
+	if (ctx.method == "GET") {
 		if (ctx.path !== "/" || ctx.query.file === undefined || ctx.query.file === "") {
-			ctx.redirect('/?file=index.html');
+			ctx.redirect("/?file=index.html");
 		} else {
 			const absPath = path.join(process.cwd(), "public", ctx.query.file);
 			try {
-				const text = fs.readFileSync(absPath, 'utf8');
+				const text = fs.readFileSync(absPath, "utf8");
 				ctx.body = tpl(absPath, text);
 			} catch (err) {
-				if (err.code == 'ENOENT') {
+				if (err.code == "ENOENT") {
 					const absPathSafe = absPath.replace(/</g, '&lt;');
 					ctx.status = 404;
 					ctx.body = tpl(absPath, `<h2>404 ERROR:</h2><p><code>${absPathSafe}</code> Not Found.</p>`);
-				} else if (err.code == 'EISDIR') {
+				} else if (err.code == "EISDIR") {
 					const absPathSafe = absPath.replace(/</g, '&lt;');
 					ctx.status = 400;
 					ctx.body = tpl(absPath, `<h2>400 ERROR:</h2><p><code>${absPathSafe}</code> Is A Directory.</p>`);
