5d2cdb1b22b104be41f91b909c65d193771531be
Harrison Katz <git@hkatz.dev>
2022-07-13 12:28:08 -0700

    feat: add directory error checking
    Adds a detailed 400 error page when the file requested is a valid directory.

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const tpl = genWeb.randomTemplate(seed);
 
 app.use(ctx => {
 	if (ctx.method == 'GET') {
-		if (ctx.path !== "/" || ctx.query.file === undefined) {
+		if (ctx.path !== "/" || ctx.query.file === undefined || ctx.query.file === "") {
 			ctx.redirect('/?file=index.html');
 		} else {
 			const absPath = path.join(process.cwd(), "public", ctx.query.file);
@@ -23,6 +23,10 @@ app.use(ctx => {
 					const absPathSafe = absPath.replace(/</g, '&lt;');
 					ctx.status = 404;
 					ctx.body = tpl(absPath, `<h2>404 ERROR:</h2><p><code>${absPathSafe}</code> Not Found.</p>`);
+				} else if (err.code == 'EISDIR') {
+					const absPathSafe = absPath.replace(/</g, '&lt;');
+					ctx.status = 400;
+					ctx.body = tpl(absPath, `<h2>400 ERROR:</h2><p><code>${absPathSafe}</code> Is A Directory.</p>`);
 				} else {
 					throw err;
 				}
